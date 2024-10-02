800f11cb1e8d47dd8912ea85ae7b5fee28f34946
thegeneral <manuelbartimeus@gmail.com>
2024-10-02 22:30:01 +0000

    Added code for food list component

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
 
-    const addToCart = () => {
+    const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev)=>({...prev,[itemId]:1}))
         }
@@ -16,10 +16,23 @@ const StoreContextProvider = (props) => {
         }
     }
 
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
 
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
