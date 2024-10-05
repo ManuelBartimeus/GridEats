3e81a8422fc75a6d96a5e94ccebc77e6854d5081
thegeneral <manuelbartimeus@gmail.com>
2024-10-05 16:16:12 +0000

    Updated menu item assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,14 +2,9 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import vendor_1 from './vendor_1.png'
+import vendor_2 from './vendor_2.png'
+import vendor_3 from './vendor_3.png'
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -42,7 +37,6 @@ import food_28 from './food_28.png'
 import food_29 from './food_29.png'
 import food_30 from './food_30.png'
 import food_31 from './food_31.png'
-import food_32 from './food_32.png'
 
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
@@ -86,267 +80,266 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
-        menu_image: menu_1
+        menu_name: "Eatery",
+        menu_image: vendor_1
     },
     {
-        menu_name: "Rolls",
-        menu_image: menu_2
+        menu_name: "VRA",
+        menu_image: vendor_2
     },
     {
-        menu_name: "Deserts",
-        menu_image: menu_3
-    },
-    {
-        menu_name: "Sandwich",
-        menu_image: menu_4
-    },
-    {
-        menu_name: "Cake",
-        menu_image: menu_5
-    },
-    {
-        menu_name: "Pure Veg",
-        menu_image: menu_6
-    },
-    {
-        menu_name: "Pasta",
-        menu_image: menu_7
-    },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
+        menu_name: "Bush Canteen",
+        menu_image: vendor_3
     }]
 
 export const food_list = [
     {
         _id: "1",
-        name: "Greek salad",
+        name: "Jollof Rice with Chicken",
         image: food_1,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
+        price: 25,
+        description: "A classic Ghanaian dish served with spicy jollof rice and tender chicken.",
+        category: "Eatery"
     },
     {
         _id: "2",
-        name: "Veg salad",
+        name: "Waakye with Fish",
         image: food_2,
-        price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
-        _id: "3",
-        name: "Clover Salad",
-        image: food_3,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
-        _id: "4",
-        name: "Chicken Salad",
-        image: food_4,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Salad"
-    }, {
-        _id: "5",
-        name: "Lasagna Rolls",
-        image: food_5,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
-        _id: "6",
-        name: "Peri Peri Rolls",
-        image: food_6,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
-        _id: "7",
-        name: "Chicken Rolls",
-        image: food_7,
         price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
-        _id: "8",
-        name: "Veg Rolls",
-        image: food_8,
+        description: "Rice and beans served with fried fish, accompanied by spicy pepper sauce.",
+        category: "VRA"
+    },
+    {
+        _id: "3",
+        name: "Banku with Okro Stew",
+        image: food_3,
+        price: 30,
+        description: "Fermented corn and cassava dough served with slimy okro stew and fish.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "4",
+        name: "Fufu with Light Soup",
+        image: food_4,
+        price: 35,
+        description: "Pounded yam or plantain served with a hot light soup and goat meat.",
+        category: "Eatery"
+    },
+    {
+        _id: "5",
+        name: "Red Red (Beans Stew) with Fried Plantain",
+        image: food_5,
+        price: 18,
+        description: "Beans stew served with crispy fried plantain slices.",
+        category: "VRA"
+    },
+    {
+        _id: "6",
+        name: "Kenkey with Fried Fish",
+        image: food_6,
         price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Rolls"
-    }, {
-        _id: "9",
-        name: "Ripple Ice Cream",
-        image: food_9,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
-        _id: "10",
-        name: "Fruit Ice Cream",
-        image: food_10,
+        description: "Fermented maize dough served with fried fish and pepper sauce.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "7",
+        name: "Gari Soakings",
+        image: food_7,
+        price: 8,
+        description: "Quick snack of soaked gari (cassava flakes) with milk and sugar.",
+        category: "Eatery"
+    },
+    {
+        _id: "8",
+        name: "Yam with Kontomire Stew",
+        image: food_8,
         price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
-        _id: "11",
-        name: "Jar Ice Cream",
-        image: food_11,
+        description: "Boiled yam served with kontomire (cocoyam leaves) stew and eggs.",
+        category: "VRA"
+    },
+    {
+        _id: "9",
+        name: "Tuo Zaafi with Ayoyo Soup",
+        image: food_9,
+        price: 28,
+        description: "Millet or maize dough served with green Ayoyo soup and beef.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "10",
+        name: "Kelewele",
+        image: food_10,
         price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
-    }, {
-        _id: "12",
-        name: "Vanilla Ice Cream",
-        image: food_12,
+        description: "Spicy fried plantains, a perfect snack or side dish.",
+        category: "Eatery"
+    },
+    {
+        _id: "11",
+        name: "Chinchinga (Grilled Kebab)",
+        image: food_11,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Deserts"
+        description: "Grilled skewers of spiced beef or chicken.",
+        category: "VRA"
+    },
+    {
+        _id: "12",
+        name: "Ga Kenkey with Shito",
+        image: food_12,
+        price: 15,
+        description: "Kenkey served with spicy black pepper sauce (Shito) and sardines.",
+        category: "Bush Canteen"
     },
     {
         _id: "13",
-        name: "Chicken Sandwich",
+        name: "Fried Rice with Chicken",
         image: food_13,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
+        price: 25,
+        description: "Stir-fried rice with vegetables served alongside grilled chicken.",
+        category: "Eatery"
     },
     {
         _id: "14",
-        name: "Vegan Sandwich",
+        name: "Kokonte with Groundnut Soup",
         image: food_14,
         price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
+        description: "Dried cassava dough served with rich peanut soup and goat meat.",
+        category: "VRA"
+    },
+    {
         _id: "15",
-        name: "Grilled Sandwich",
+        name: "Angwamo (Fried Rice) with Fried Egg",
         image: food_15,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
-        _id: "16",
-        name: "Bread Sandwich",
-        image: food_16,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Sandwich"
-    }, {
-        _id: "17",
-        name: "Cup Cake",
-        image: food_17,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "18",
-        name: "Vegan Cake",
-        image: food_18,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "19",
-        name: "Butterscotch Cake",
-        image: food_19,
-        price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "20",
-        name: "Sliced Cake",
-        image: food_20,
-        price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Cake"
-    }, {
-        _id: "21",
-        name: "Garlic Mushroom ",
-        image: food_21,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
-        _id: "22",
-        name: "Fried Cauliflower",
-        image: food_22,
-        price: 22,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
-        _id: "23",
-        name: "Mix Veg Pulao",
-        image: food_23,
         price: 10,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
-    }, {
+        description: "Simple Ghanaian fried rice served with fried eggs.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "16",
+        name: "Abolo with Grilled Tilapia",
+        image: food_16,
+        price: 22,
+        description: "Cornmeal dough served with spiced grilled tilapia.",
+        category: "Eatery"
+    },
+    {
+        _id: "17",
+        name: "Soft Beverages",
+        image: food_17,
+        price: 5,
+        description: "Range of soft beverages with classic tastes.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "18",
+        name: "Sobolo (Hibiscus Drink)",
+        image: food_18,
+        price: 6,
+        description: "Chilled hibiscus drink, rich in vitamins and antioxidants.",
+        category: "VRA"
+    },
+    {
+        _id: "19",
+        name: "Asaana",
+        image: food_19,
+        price: 7,
+        description: "Sweet fermented corn drink, commonly known as 'elephant beer'.",
+        category: "Eatery"
+    },
+    {
+        _id: "20",
+        name: "Brukina",
+        image: food_20,
+        price: 8,
+        description: "Millet and yogurt mix, a refreshing and filling beverage.",
+        category: "VRA"
+    },
+    {
+        _id: "21",
+        name: "Bissap",
+        image: food_21,
+        price: 6,
+        description: "Another version of Sobolo, made from dried hibiscus flowers.",
+        category: "Bush Canteen"
+    },
+    {
+        _id: "22",
+        name: "Boiled Plantain with Palava Sauce",
+        image: food_22,
+        price: 14,
+        description: "Boiled ripe or unripe plantain served with spinach-based palava sauce.",
+        category: "Eatery"
+    },
+    {
+        _id: "23",
+        name: "Ampesi with Garden Egg Stew",
+        image: food_23,
+        price: 20,
+        description: "Boiled yam or plantain served with garden egg (eggplant) stew.",
+        category: "VRA"
+    },
+    {
         _id: "24",
-        name: "Rice Zucchini",
+        name: "Apapransa",
         image: food_24,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pure Veg"
+        price: 18,
+        description: "A traditional cornmeal dish mixed with palm nut soup and beans.",
+        category: "Bush Canteen"
     },
     {
         _id: "25",
-        name: "Cheese Pasta",
+        name: "Etsew with Fish Stew",
         image: food_25,
-        price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
+        price: 22,
+        description: "Steamed white rice served with a spicy fish stew.",
+        category: "Eatery"
     },
     {
         _id: "26",
-        name: "Tomato Pasta",
+        name: "Atadwe Milk",
         image: food_26,
-        price: 18,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
+        price: 6,
+        description: "A nutritious drink made from tiger nuts and milk.",
+        category: "VRA"
+    },
+    {
         _id: "27",
-        name: "Creamy Pasta",
+        name: "Fante Kenkey with Pepper and Fried Fish",
         image: food_27,
-        price: 16,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
+        price: 15,
+        description: "Kenkey served with fried fish and hot pepper sauce.",
+        category: "Bush Canteen"
+    },
+    {
         _id: "28",
-        name: "Chicken Pasta",
+        name: "Bofrot (Puff Puff)",
         image: food_28,
-        price: 24,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Pasta"
-    }, {
+        price: 4,
+        description: "Sweet fried dough balls, a popular Ghanaian snack.",
+        category: "Eatery"
+    },
+    {
         _id: "29",
-        name: "Buttter Noodles",
+        name: "Kokonte with Palm Nut Soup",
         image: food_29,
-        price: 14,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
+        price: 18,
+        description: "Kokonte served with thick and rich palm nut soup.",
+        category: "VRA"
+    },
+    {
         _id: "30",
-        name: "Veg Noodles",
+        name: "Tatale with Bean Stew",
         image: food_30,
         price: 12,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
+        description: "Fried ripe plantain cakes served with bean stew.",
+        category: "Eatery"
+    },
+    {
         _id: "31",
-        name: "Somen Noodles",
+        name: "Koose (Bean Cakes)",
         image: food_31,
-        price: 20,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
-    }, {
-        _id: "32",
-        name: "Cooked Noodles",
-        image: food_32,
-        price: 15,
-        description: "Food provides essential nutrients for overall health and well-being",
-        category: "Noodles"
+        price: 5,
+        description: "Deep-fried spiced black-eyed pea cakes, often served as a snack.",
+        category: "Bush Canteen"
     }
 ]
+
